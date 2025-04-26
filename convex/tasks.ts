f909f0969be71b9f197674dfe4c4df8e736c74de
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-04-26 23:47:08 +1200

    tasks bug fixed

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -2,7 +2,7 @@ import { query } from "./_generated/server";
 
 export const get = query({
   args: {},
-  handler: async (ctx) => {
+  handler: async (ctx: any) => {
     return await ctx.db.query("tasks").collect();
   },
 });
