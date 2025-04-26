9ab2a82958886b7526dfe71d890c5ee4a31f7afb
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-04-26 20:39:38 +1200

    convex/auth and convex/core libraries added in project

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { useQuery } from 'convex/react';
+import { api } from '../../../convex/_generated/api';
+
 const Page = () => {
+    const tasks = useQuery(api.tasks.get);
     return (
-        <div>
-            Test page
-        </div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+        </main>
     )
 }
 
