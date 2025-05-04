664a321e7612a8457e716d99bbda1902f09010dd
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-05-04 22:11:13 +1200

    bug fixed and ConvexAuthNExtjsProvider component and middleware just added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
         <ConvexClientProvider>
           {children}
         </ConvexClientProvider>
-        
       </body>
     </html>
     </ConvexAuthNextjsServerProvider>
--- src/components/convex-client-provider.tsx
+++ src/components/convex-client-provider.tsx
@@ -2,10 +2,15 @@
 
 import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
-import {ConvexAuthNextJsProvider} from '@convex-dev/auth/nextjs';
+import { ConvexAuthNextjsProvider } from '@convex-dev/auth/nextjs';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexAuthNextJsProvider client={convex}>{children}</ConvexAuthNextJsProvider>;
+  return (
+    <ConvexAuthNextjsProvider client={convex}>
+      {children}
+    </ConvexAuthNextjsProvider>
+  );
+    
 }
